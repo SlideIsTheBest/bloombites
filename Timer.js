65cf94dfb09ea0770d124b289d10b72ee2d1d146
Slide <slide2333rblx@gmail.com>
2025-12-01 16:55:58 +0700

    Refactor countdown timer logic

--- Timer.js
+++ Timer.js
@@ -1,7 +1,6 @@
 const timer = document.getElementById("timer");
 const timer_mobile = document.getElementById("timer_mobile");
 const preorder = document.getElementById("preorder");
-const show_preorder = document.querySelector("#show_product_display button");
 
 const start_date = new Date(2025, 11, 3, 0, 0, 0).getTime();  // Month starts with 0 (January)
 const end_date = new Date(2025, 11, 10, 23, 59, 59).getTime();  // Month starts with 0 (January)
@@ -29,13 +28,11 @@ const countdown = setInterval(() => {
       timer_mobile.innerHTML = `Opening in ${hours}h ${minutes}m ${seconds}s`; 
     }
     preorder.disabled = true;
-    show_preorder.disabled = true;
     return;
   } else if (end_diff <= 0) {
     timer.innerHTML = "Coming Soon!";
     timer_mobile.innerHTML = "Coming Soon!";
     preorder.disabled = true;
-    show_preorder.disabled = true;
     clearInterval(countdown);
     return;
   }
@@ -59,3 +56,4 @@ const countdown = setInterval(() => {
   };
 
 }, 1000);
+
